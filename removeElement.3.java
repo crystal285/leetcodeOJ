d43cfc2d4dc019275e9d18d58e7b3e6fa22f5344
crystal285 <jing.meng278@gmail.com>
2015-06-03 21:46:06 -0700

    removeElement

---
+++ removeElement.3.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = 0;
+        while(right < nums.length)
+        {
+            if(nums[right] == val)
+            {
+                right++;
+            }
+            else
+            {
+                nums[left] = nums[right];
+                left++;
+                right++;
+            }
+        }
+        return left;
+    }
+}
