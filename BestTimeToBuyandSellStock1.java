8ddb002438d95ce08d233319b696172b6c591375
crystal285 <jing.meng278@gmail.com>
2015-08-17 17:18:47 -0700

    BestTimeToBuyandSellStock1.java

---
+++ BestTimeToBuyandSellStock1.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++)
+        {
+            if(prices[i] < min)
+                min = prices[i];
+            profit = profit > prices[i] - min? profit : prices[i] - min;
+        }
+        return profit;
+    }
+}
