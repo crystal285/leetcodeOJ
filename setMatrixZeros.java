ca9f87ec699b789173b99e9e9b4a109d7479c416
crystal285 <jing.meng278@gmail.com>
2015-09-21 17:28:56 -0700

    setMatrixZeros

---
+++ setMatrixZeros.java
@@ -0,0 +1,50 @@
+//space O(1)
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstcol = false;
+        boolean firstrow = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 0; i < m; i++)
+        {
+            if(matrix[i][0] == 0)
+            {
+                firstcol = true;
+                break;
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(matrix[0][i] == 0)
+            {
+                firstrow = true;
+                break;
+            }
+        }
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        if(firstcol == true)
+        {
+            for(int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+        if(firstrow == true)
+        {
+            for(int i = 0; i < n; i++)
+                matrix[0][i] = 0;
+        }
+    }
+}
