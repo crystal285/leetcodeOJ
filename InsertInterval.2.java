c86fecd2779e7a420595136059e0fcb2c7716849
crystal285 <jing.meng278@gmail.com>
2015-10-09 19:08:16 -0700

    InsertInterval.2.java

---
+++ InsertInterval.2.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+ //more tricky and concise version
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        ArrayList<Interval> lists = new ArrayList<Interval>();
+        for(Interval t: intervals)
+        {
+            if(t.end < newInterval.start)
+            {
+                lists.add(t);
+            }
+            else if(newInterval.end < t.start)
+            {
+                lists.add(newInterval);
+                newInterval = t;
+            }
+            else if(newInterval.end >= t.start)
+            {
+                newInterval = new Interval(Math.min(t.start, newInterval.start), 
+                Math.max(newInterval.end, t.end));
+            }
+        }
+        lists.add(newInterval);
+        return lists;
+    }
+}
